0df5735529dd6bfd1d082fc6410fabaf7cc000d0
fauzanashshidiq <muhzeen07@gmail.com>
2025-09-26 14:21:07 +0700

    feat(main): intent for activity_main2

--- app/src/main/java/com/example/hellotoast/MainActivity.kt
+++ app/src/main/java/com/example/hellotoast/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.hellotoast
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
@@ -19,6 +20,7 @@ class MainActivity : AppCompatActivity() {
         val mShowCount = findViewById<TextView>(R.id.show_count)
         val buttonCountUp = findViewById<Button>(R.id.button_count)
         val buttonToast = findViewById<Button>(R.id.button_toast)
+        val buttonSwitchPage = findViewById<Button>(R.id.button_switchpage)
 
         buttonCountUp.setOnClickListener(View.OnClickListener{
             mCount++
@@ -29,6 +31,10 @@ class MainActivity : AppCompatActivity() {
             val tulisan: String = mShowCount?.text.toString()
             val toast: Toast = Toast.makeText(this, "Angka yang dimunculkan: " + tulisan, Toast.LENGTH_LONG)
             toast.show()
+        })
+        buttonSwitchPage.setOnClickListener(View.OnClickListener{
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
         })
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
